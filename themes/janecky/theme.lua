34ff4a3586e44313c42a74f88f7b689ff674f6eb
Janne Veteläinen <janne.vetelainen@elisanet.fi>
2022-09-28 22:56:31 +0300

    Remove lain, fix theme

--- themes/janecky/theme.lua
+++ themes/janecky/theme.lua
@@ -66,7 +66,7 @@ theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
 -- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
-theme.menu_submenu_icon = themes_path.."default/submenu.png"
+theme.menu_submenu_icon = themes_path.."janecky/submenu.png"
 theme.menu_height = dpi(15)
 theme.menu_width  = dpi(100)
 
@@ -76,57 +76,57 @@ theme.menu_width  = dpi(100)
 --theme.bg_widget = "#cc0000"
 
 -- Define the image to load
-theme.titlebar_close_button_normal = themes_path.."default/titlebar/redcircle.png"
-theme.titlebar_close_button_focus  = themes_path.."default/titlebar/redcircle.png"
+theme.titlebar_close_button_normal = themes_path.."janecky/titlebar/redcircle.png"
+theme.titlebar_close_button_focus  = themes_path.."janecky/titlebar/redcircle.png"
 
-theme.titlebar_floating_button_normal_inactive = themes_path.."default/titlebar/yellowcircle.png"
-theme.titlebar_floating_button_focus_inactive  = themes_path.."default/titlebar/yellowcircle.png"
-theme.titlebar_floating_button_normal_active = themes_path.."default/titlebar/yellowcircle.png"
-theme.titlebar_floating_button_focus_active  = themes_path.."default/titlebar/yellowcircle.png"
+theme.titlebar_floating_button_normal_inactive = themes_path.."janecky/titlebar/yellowcircle.png"
+theme.titlebar_floating_button_focus_inactive  = themes_path.."janecky/titlebar/yellowcircle.png"
+theme.titlebar_floating_button_normal_active = themes_path.."janecky/titlebar/yellowcircle.png"
+theme.titlebar_floating_button_focus_active  = themes_path.."janecky/titlebar/yellowcircle.png"
 
-theme.titlebar_ontop_button_normal_inactive = themes_path.."default/titlebar/greencircle.png"
-theme.titlebar_ontop_button_focus_inactive  = themes_path.."default/titlebar/greencircle.png"
-theme.titlebar_ontop_button_normal_active = themes_path.."default/titlebar/greencircle.png"
-theme.titlebar_ontop_button_focus_active  = themes_path.."default/titlebar/greencircle.png"
+theme.titlebar_ontop_button_normal_inactive = themes_path.."janecky/titlebar/greencircle.png"
+theme.titlebar_ontop_button_focus_inactive  = themes_path.."janecky/titlebar/greencircle.png"
+theme.titlebar_ontop_button_normal_active = themes_path.."janecky/titlebar/greencircle.png"
+theme.titlebar_ontop_button_focus_active  = themes_path.."janecky/titlebar/greencircle.png"
 
 --[[
-theme.titlebar_minimize_button_normal = themes_path.."default/titlebar/minimize_normal.png"
-theme.titlebar_minimize_button_focus  = themes_path.."default/titlebar/minimize_focus.png"
+theme.titlebar_minimize_button_normal = themes_path.."janecky/titlebar/minimize_normal.png"
+theme.titlebar_minimize_button_focus  = themes_path.."janecky/titlebar/minimize_focus.png"
 --]]
 
 --[[
-theme.titlebar_sticky_button_normal_inactive = themes_path.."default/titlebar/sticky_normal_inactive.png"
-theme.titlebar_sticky_button_focus_inactive  = themes_path.."default/titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_active = themes_path.."default/titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_active  = themes_path.."default/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_inactive = themes_path.."janecky/titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive  = themes_path.."janecky/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = themes_path.."janecky/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active  = themes_path.."janecky/titlebar/sticky_focus_active.png"
 --]]
 
 --[[
-theme.titlebar_maximized_button_normal_inactive = themes_path.."default/titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_inactive = themes_path.."janecky/titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive  = themes_path.."janecky/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = themes_path.."janecky/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active  = themes_path.."janecky/titlebar/maximized_focus_active.png"
 --]]
 
 theme.wallpaper = gfs.get_random_file_from_dir(wallpapers_path, { "jpg", "png" }, true)
 
 -- You can use your own layout icons like this:
-theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
-theme.layout_fairv = themes_path.."default/layouts/fairvw.png"
-theme.layout_floating  = themes_path.."default/layouts/floatingw.png"
-theme.layout_magnifier = themes_path.."default/layouts/magnifierw.png"
-theme.layout_max = themes_path.."default/layouts/maxw.png"
-theme.layout_fullscreen = themes_path.."default/layouts/fullscreenw.png"
-theme.layout_tilebottom = themes_path.."default/layouts/tilebottomw.png"
-theme.layout_tileleft   = themes_path.."default/layouts/tileleftw.png"
-theme.layout_tile = themes_path.."default/layouts/tilew.png"
-theme.layout_tiletop = themes_path.."default/layouts/tiletopw.png"
-theme.layout_spiral  = themes_path.."default/layouts/spiralw.png"
-theme.layout_dwindle = themes_path.."default/layouts/dwindlew.png"
-theme.layout_cornernw = themes_path.."default/layouts/cornernww.png"
-theme.layout_cornerne = themes_path.."default/layouts/cornernew.png"
-theme.layout_cornersw = themes_path.."default/layouts/cornersww.png"
-theme.layout_cornerse = themes_path.."default/layouts/cornersew.png"
+theme.layout_fairh = themes_path.."janecky/layouts/fairhw.png"
+theme.layout_fairv = themes_path.."janecky/layouts/fairvw.png"
+theme.layout_floating  = themes_path.."janecky/layouts/floatingw.png"
+theme.layout_magnifier = themes_path.."janecky/layouts/magnifierw.png"
+theme.layout_max = themes_path.."janecky/layouts/maxw.png"
+theme.layout_fullscreen = themes_path.."janecky/layouts/fullscreenw.png"
+theme.layout_tilebottom = themes_path.."janecky/layouts/tilebottomw.png"
+theme.layout_tileleft   = themes_path.."janecky/layouts/tileleftw.png"
+theme.layout_tile = themes_path.."janecky/layouts/tilew.png"
+theme.layout_tiletop = themes_path.."janecky/layouts/tiletopw.png"
+theme.layout_spiral  = themes_path.."janecky/layouts/spiralw.png"
+theme.layout_dwindle = themes_path.."janecky/layouts/dwindlew.png"
+theme.layout_cornernw = themes_path.."janecky/layouts/cornernww.png"
+theme.layout_cornerne = themes_path.."janecky/layouts/cornernew.png"
+theme.layout_cornersw = themes_path.."janecky/layouts/cornersww.png"
+theme.layout_cornerse = themes_path.."janecky/layouts/cornersew.png"
 
 -- Generate Awesome icon:
 theme.awesome_icon = theme_assets.awesome_icon(
--- widget.lua
+++ widget.lua
@@ -5,11 +5,10 @@ local wibox = require("wibox")
 local _M = {}
 
 local interval = 2
-local color = "#5E81AC"
 
 -- Clock wibar widget
 _M.clock = wibox.widget({
-    format = "<span foreground='#5E81AC' size='large'> %d/%m %H:%M </span>",
+    format = "<span foreground='#2E3440' size='large'> %d/%m %H:%M </span>",
     widget = wibox.widget.textclock
 })
 
@@ -17,7 +16,7 @@ _M.clock = wibox.widget({
 _M.cputemp = awful.widget.watch(
     "sensors -u", interval, function(widget, stdout)
         local out = stdout.match(stdout, "temp1_input: (%d+)%.")
-        out = out.format("<span foreground='%s' size='large'> cpu: %dc </span>", color, out)
+        out = out.format("<span foreground='%s' size='large'> cpu: %dc </span>", x.nord0, out)
         widget:set_markup(out)
     end
 )
@@ -25,7 +24,7 @@ _M.cputemp = awful.widget.watch(
 -- Nvidia gpu temperature wibar widget
 _M.gputemp = awful.widget.watch(
     "nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader", interval, function(widget, stdout)
-        local out = stdout.format("<span foreground='%s' size='large'> gpu: %dc </span>", color, stdout)
+        local out = stdout.format("<span foreground='%s' size='large'> gpu: %dc </span>", x.nord0, stdout)
         widget:set_markup(out)
     end
 )
@@ -34,7 +33,7 @@ _M.gputemp = awful.widget.watch(
 _M.sysload = awful.widget.watch(
     "cat /proc/loadavg", interval, function(widget, stdout)
         local out = stdout.match(stdout, "([^%s]+ [^%s]+ [^%s]+)")
-        out = out.format("<span foreground='%s' size='large'> %s </span>", color, out)
+        out = out.format("<span foreground='%s' size='large'> %s </span>", x.nord0, out)
         widget:set_markup(out)
     end
 )
@@ -44,7 +43,7 @@ _M.diskfree = awful.widget.watch(
     --"sh -c 'df -h --output=avail /home | sed -r '1d;s/\\s+//g''", interval, function(widget, stdout)
     "df -h --output=avail /home", interval, function(widget, stdout)
         local out = stdout.match(stdout, "(%d+G)")
-        out = out.format("<span foreground='%s' size='large'> df: %s </span>", color, out)
+        out = out.format("<span foreground='%s' size='large'> df: %s </span>", x.nord0, out)
         widget:set_markup(out)
     end
 )
